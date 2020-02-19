c4cad4084e71a8b97c1212ad36120fcca98bf55e
Tarek Aouadhi <tarekaouadhi@gmail.com>
2020-02-19 15:01:23 +0100

    Add project files.

---
+++ US_Whal_Grafisch/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using US_Whal_Grafisch.model;
+
+namespace US_Whal_Grafisch
+{
+    public partial class Form1 : Form
+    {
+
+        static List<Wähler> wl2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Wähler> wl = new List<Wähler>();
+            List<Wähler> wl2 = Model.ZeigeWahlvolk(wl);
+
+            
+
+            
+        }
+
+
+        public class Parameter
+        {
+            public Geschlecht Geschlecht { get; set; } 
+            public PolitischeHeimat PolitischeHeimat { get; set; }
+          
+        }
+
+
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string geschlecht = comboBox1.Text;
+            string ph = comboBox2.Text;
+
+
+
+            Parameter p = new Parameter();
+
+            switch (geschlecht)
+            {
+                case "Maenlich":
+                    p.Geschlecht = Geschlecht.Maenlich;
+                    break;
+
+                case "Weiblich":
+                    p.Geschlecht = Geschlecht.Weiblich;
+                    break;
+            }
+
+            switch (ph)
+            {
+                case "Demokraten":
+                    p.PolitischeHeimat = PolitischeHeimat.Demokraten;
+                    break;
+
+                case "Republikaner":
+                    p.PolitischeHeimat = PolitischeHeimat.Republikaner;
+                    break;
+            }
+
+            List<Wähler> wl_link = Program.Filtern(wl2, p);
+
+
+
+            textBox1.Text = "";
+            foreach (var item in wl_link)
+            {
+                textBox1.AppendText("Vorname: " + item.Vorname + " ," +
+                                     "Nachname: " + item.Nachname + " ," +
+                                     "PLZ: " + item.PLZ + " ," +
+                                     "Politische Heimat: " + item.PolitischeHeimat + " ," +
+                                     "Alter: " + item.Alter + " ," +
+                                     "Geschlecht: " + item.Geschlecht + " ," +
+                                     "Beeinflussbarkeit: " + item.Beeinflussbarkeit + " ," +
+                                     "Schicht: " + item.Schicht + "\n"
+                                     );
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ US_Whal_Grafisch/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using US_Whal_Grafisch.model;
+
+namespace US_Whal_Grafisch
+{
+
+    public enum Geschlecht { Weiblich, Maenlich }
+    public enum Beeinflussbarkeit { Leicht, Mittel, Schwer }
+    public enum Alter { Erstwaehler, Bis30, Bis40, Bis50, Restliche }
+    public enum Schicht { Unterschicht, Unteremittelschicht, Oberemittelschicht, Oberschicht }
+    public enum PolitischeHeimat { Republikaner, Demokraten }
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+            Console.ReadLine();
+        }
+
+
+        public static List<Wähler> Filtern(Form1.Parameter p)
+        {
+            List<Wähler> wl_link = new List<Wähler>();
+
+            FileStream fs9 = File.Open("us_wahl_liste.txt", FileMode.Open);
+            StreamReader sr9 = new StreamReader(fs9);
+            string line;
+            while ((line = sr9.ReadLine()) != null)
+            {
+              // hier muss noch aus der Datei ausgelesen werden und objekte vom typ Wähler in einer List<Wähler> gespeichert werden
+            }
+            fs9.Close();
+
+            var wl_link = from wähler in wl
+                          where
+                              wähler.PolitischeHeimat == p.PolitischeHeimat &&
+                              wähler.Geschlecht       == p.Geschlecht 
+
+                              //wähler.Alter == Alter.Erstwaehler &&
+                              //wähler.Schicht == Schicht.OBERSCHICHT &&
+
+                              //wähler.PLZ > 47111 &&
+                              //wähler.PLZ < 80000 &&
+                              //&&
+                              //wähler.Beeinflussbarkeit == Beeinflussbarkeit.Leicht
+                              select wähler;
+
+            return wl_link.ToList();
+        }
+    }
+}
